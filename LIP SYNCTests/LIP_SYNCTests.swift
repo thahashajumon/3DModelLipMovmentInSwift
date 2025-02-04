caf66bee24e7e2d7b21db73d035c254e3daca538
Thaha <shajumon@actionfi.com>
2025-02-04 12:38:37 +0530

    Initial Commit

---
+++ LIP SYNCTests/LIP_SYNCTests.swift
@@ -0,0 +1,17 @@
+//
+//  LIP_SYNCTests.swift
+//  LIP SYNCTests
+//
+//  Created by actionfi on 04/02/25.
+//
+
+import Testing
+@testable import LIP_SYNC
+
+struct LIP_SYNCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
